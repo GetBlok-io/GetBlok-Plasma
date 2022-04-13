c78dea9045ae792a56902670508ddccd11259ae8
K-Singh <kiratgsingh@gmail.com>
2022-04-13 03:56:52 -0400

    Changed hexMapping implementation

--- src/main/scala/persistence/VersionedPlasmaMap.scala
+++ src/main/scala/persistence/VersionedPlasmaMap.scala
@@ -24,8 +24,8 @@ class VersionedPlasmaMap(val versionedByteMap: ListMap[ADDigest, ListMap[Key, Va
 }
 
 object VersionedPlasmaMap {
-  type Key = Array[Byte] @@ ADKey.Tag
-  type Value = Array[Byte] @@ ADValue
+  type Key    = Array[Byte] @@ ADKey.Tag
+  type Value  = Array[Byte] @@ ADValue
 
   /** Default mapping takes array of bytes and tags them */
   def fromByteMappings(byteMappings: ListMap[Array[Byte], ListMap[Array[Byte], Array[Byte]]]): VersionedPlasmaMap = {
@@ -37,9 +37,8 @@ object VersionedPlasmaMap {
 
   /** Takes mappings of hex string and builds them into VersionedPlasmaMap */
   def fromHexMapping(hexMappings: ListMap[String, ListMap[String, String]]): VersionedPlasmaMap = {
-    val byteMappings = hexMappings.map(vers => BigInt(vers._1).toByteArray ->
-      ListMap(vers._2.map(maps => BigInt(maps._1).toByteArray -> BigInt(maps._2).toByteArray).toArray:_*))
-    fromByteMappings(byteMappings)
+    val byteMappings = fromByteConversion(hexMappings)
+    byteMappings
   }
 
   /**
